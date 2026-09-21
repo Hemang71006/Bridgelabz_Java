37a4d7152cb22946a7d18758732b8a6c8a6e2409
Hemang <hemangjeswani07@gmail.com>
2026-09-21 17:12:54 +0530

    [Hemang]:Added Level 2 lab practices

---
+++ Bridgelabz_Java/src/main/java/javaProgrammingElements/level2/FahrenheitToCelsius.java
@@ -0,0 +1,20 @@
+package javaProgrammingElements.level2;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+
+        double celsiusResult = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("The " + fahrenheit +
+                " Fahrenheit is " + celsiusResult + " Celsius");
+
+        input.close();
+    }
+}
---
+++ Bridgelabz_Java/src/main/java/javaProgrammingElements/level2/TravelComputation.java
@@ -0,0 +1,44 @@
+package javaProgrammingElements.level2;
+
+import java.util.Scanner;
+
+public class TravelComputation {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter from city: ");
+        String fromCity = input.nextLine();
+
+        System.out.print("Enter via city: ");
+        String viaCity = input.nextLine();
+
+        System.out.print("Enter to city: ");
+        String toCity = input.nextLine();
+
+        System.out.print("Enter distance from " + fromCity + " to " + viaCity + ": ");
+        double distanceFromToVia = input.nextDouble();
+
+        System.out.print("Enter distance from " + viaCity + " to " + toCity + ": ");
+        double distanceViaToFinalCity = input.nextDouble();
+
+        System.out.print("Enter time from " + fromCity + " to " + viaCity + " in minutes: ");
+        int timeFromToVia = input.nextInt();
+
+        System.out.print("Enter time from " + viaCity + " to " + toCity + " in minutes: ");
+        int timeViaToFinalCity = input.nextInt();
+
+        double totalDistance = distanceFromToVia + distanceViaToFinalCity;
+        int totalTime = timeFromToVia + timeViaToFinalCity;
+
+        System.out.println("The Total Distance travelled by " + name +
+                " from " + fromCity + " to " + toCity +
+                " via " + viaCity + " is " + totalDistance +
+                " km and the Total Time taken is " + totalTime + " minutes");
+
+        input.close();
+    }
+}
